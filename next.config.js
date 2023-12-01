ac62be92b86d5a058418b96bac39f76cb58612d8
Rishabh@iv5394 <boletorishabh1999@gmail.com>
2023-12-01 15:01:43 +0530

    add glide

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
+const { parsed } = require('dotenv').config();
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
-        pathname: "/assets/**",
-      },
-    ],
-  },
-
-  experimental: {
-    serverActions: true,
-  },
   images: {
     // domains: ['https://kreomart.onrender.com/api/products/'],
     remotePatterns: [
@@ -31,8 +15,23 @@ const nextConfig = {
         hostname: "kreomart.onrender.com",
         pathname: "/media/**",
       },
+      {
+        protocol: "https",
+        hostname: "kreomart.s3.ap-south-1.amazonaws.com",
+        pathname: "/**",
+
+      }
     
     ],
+  },
+  env: {
+    NEXT_PUBLIC_API_ROOT: parsed.NEXT_PUBLIC_API_ROOT,
+    GITHUB_CLIENT_ID: parsed.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: parsed.GITHUB_CLIENT_SECRET,
+    GOOGLE_CLIENT_ID: parsed.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: parsed.GOOGLE_CLIENT_SECRET,
+    JWT_SECRET: parsed.JWT_SECRET,
+    NEXTAUTH_SECRET: parsed.NEXTAUTH_SECRET,
   },
 };
 
